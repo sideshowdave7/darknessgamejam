62e914ead854d38ac5440b6a20790fc8dc47237e
David Hurst <sideshowdave7@gmail.com>
2013-09-21 11:03:15 -0600

    add cockroach anim prefab

---
+++ Assets/DarknessAudioManager.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class DarknessAudioManager  : MonoBehaviour
+{
+	
+	public List<AudioClip> audioClips;
+	private Dictionary<AudioClip,bool> audioClipsPlaying;
+	private List<AudioSource> _audioSources = null;
+	private List<AudioSource> _deleted = null;
+	private static DarknessAudioManager _instance = null;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_audioSources = new List<AudioSource> ();
+		audioClipsPlaying = new Dictionary<AudioClip, bool>();
+		
+		foreach ( var clip in audioClips) {
+			audioClipsPlaying[clip]  = false;	
+		}
+		
+		_deleted = new List<AudioSource> ();
+		DontDestroyOnLoad(this);
+		
+		
+		foreach (var clip in audioClips){
+		
+			PlayOneShot ("Darkness-bass1","AudioManager",true);
+			PlayOneShot ("Darkness-bass2","AudioManager",true);
+			PlayOneShot ("Darkness-bass3","AudioManager",true);
+			PlayOneShot ("Darkness-melody","AudioManager",true);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	void FixedUpdate ()
+	{
+		
+		_deleted.Clear ();
+		
+		for (var x=0; x < _audioSources.Count; x++) {
+			if (_audioSources [x] != null && _audioSources [x].clip != null && !_audioSources [x].isPlaying) {
+				_deleted.Add (_audioSources [x]);
+				Destroy (_audioSources [x]);
+				audioClipsPlaying[_audioSources[x].clip] = false;
+			} 
+		}
+		
+		foreach (var _as in _deleted) {
+			_audioSources.Remove (_as);	
+		}
+	
+		
+	}
+	
+	public static DarknessAudioManager Instance {
+		get { return _instance; }
+	}
+	
+	void Awake ()
+	{
+		if (_instance != null && _instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			_instance = this;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	public void PlayOneShot (string sfxName, string targetTag, bool loop)
+	{
+		var clip = findAudioClip (sfxName);
+		if (!audioClipsPlaying[clip]){			
+			var go = GameObject.FindGameObjectWithTag (targetTag);
+		
+			AudioSource _as = (AudioSource)go.AddComponent ("AudioSource");
+			_audioSources.Add (_as);
+	
+			_as.clip = clip;
+			_as.loop = loop;
+			_as.volume = .5f;
+			_as.Play ();
+			audioClipsPlaying[clip] = true;
+		} 
+		
+	}
+	
+	AudioClip findAudioClip (string name)
+	{
+		
+		foreach (var clip in audioClips) {
+			if (clip != null)
+			{
+				if (clip.name == name) {
+					return clip;	
+				}
+			}
+		}
+		return null;
+	}
+	
+	IEnumerator FadeMusicEnum(AudioSource _as,float target)
+	{
+	    while(_as.volume > .1F)
+	    {
+	        _as.volume = Mathf.Lerp(_as.volume,target,Time.deltaTime);
+	        yield return 0;
+	    }
+	    _as.volume = 0;
+	   
+	}
+	
+	public void FadeMusic(AudioSource _as, float target)
+	{
+	    StartCoroutine(FadeMusicEnum(_as,target));
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float move_speed;
+	public PlayerPowerEnum currentPower = PlayerPowerEnum.Normal;
+	private bool prevTogglePower;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		prevTogglePower = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var input = new Vector3(Input.GetAxis ("Horizontal"),0f, Input.GetAxis ("Vertical"));
+		var togglePower = Input.GetButtonDown("TogglePower");
+		
+		if (togglePower && !prevTogglePower){
+			currentPower = (PlayerPowerEnum)(((int)currentPower + 1) % (int)PlayerPowerEnum.End);
+		}
+		
+		this.rigidbody.velocity = (Vector3.Normalize(input) * move_speed);
+		
+		prevTogglePower = togglePower;
+	}
+	
+	
+	
+	public enum PlayerPowerEnum {
+		
+		Normal,
+		Visual,
+		Audio,
+		Time,
+		End
+	};
+		
+}
