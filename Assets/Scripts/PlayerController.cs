894da07149efa16b080254477e55ccdf209abc2d
Andrew Jones <andrew.b.jones@gmail.com>
2013-09-20 22:07:28 -0600

    Derp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class PlayerController : MonoBehaviour
+{
+	public float move_speed;
 	
+	// Use this for initialization
+	void Start ()
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		var input = new Vector3(Input.GetAxis ("Horizontal"),Input.GetAxis ("Vertical"), 0f);
+		
+		this.transform.position += (input * move_speed * Time.deltaTime);
 	}
 }
