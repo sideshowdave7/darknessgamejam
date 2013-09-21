8791c53f5dc24ff54427a6885cedd1cb765f6c41
David Hurst <sideshowdave7@gmail.com>
2013-09-21 08:33:45 -0600

    bye bye

--- Assets/Scripts/PlayerPowerEnum.cs
+++ Assets/Scripts/PlayerPowerEnum.cs
@@ -1,15 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+﻿
