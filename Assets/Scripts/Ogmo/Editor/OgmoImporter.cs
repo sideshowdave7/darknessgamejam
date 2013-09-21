31bc8d4044a91a5477671d36470ce194b47601d0
Brian Paulsen <Brian_Paulsen123@hotmail.com>
2013-09-21 15:38:57 -0600

    no message

---
+++ Assets/Scripts/Ogmo/Editor/OgmoImporter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+ 
+public class OgmoImporter : AssetPostprocessor
+{
+	public static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets) {
+			if (asset.EndsWith (".oel")) {
+				string filePath = asset.Substring (0, asset.Length - Path.GetFileName (asset).Length) + "Generated Assets/";
+				string newFileName = filePath + Path.GetFileNameWithoutExtension (asset) + ".txt";
+ 
+				if (!Directory.Exists (filePath)) {
+					Directory.CreateDirectory (filePath);
+				}
+ 
+				StreamReader reader = new StreamReader (asset);
+				string fileData = reader.ReadToEnd ();
+				reader.Close ();
+ 
+				FileStream resourceFile = new FileStream (newFileName, FileMode.Create, FileAccess.Write);
+				StreamWriter writer = new StreamWriter (resourceFile);
+				writer.Write (fileData);
+				writer.Close (); 
+				resourceFile.Close ();
+ 
+				AssetDatabase.Refresh (ImportAssetOptions.Default);
+    
+				FileUtil.DeleteFileOrDirectory (filePath + Path.GetFileNameWithoutExtension (asset) + "_possibleSizes.txt");
+			}
+		}
+	}
+ 
+}
---
+++ Assets/Scripts/Ogmo/OgmoLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class OgmoLevel
+{
+	public string name;
+	public int width;
+	public int height;
+	public Dictionary<string,OgmoLayer> layers;
+
+	public OgmoLevel (TextAsset xmlFile)
+	{
+		name = xmlFile.name;
+		XmlDocument xml = new XmlDocument ();
+		xml.LoadXml (xmlFile.text);
+		XmlNode root = xml.FirstChild;
+		width = System.Convert.ToInt32 (root.Attributes.GetNamedItem ("width").Value);
+		height = System.Convert.ToInt32 (root.Attributes.GetNamedItem ("height").Value);
+		XmlNodeList children = root.ChildNodes;
+		layers = new Dictionary<string,OgmoLayer> ();
+		for (int ii = 0; ii < children.Count; ii++) {
+			OgmoLayer layer = new OgmoLayer (children [ii], width, height);
+			layers [layer.name] = layer;
+		}
+	}
+}
