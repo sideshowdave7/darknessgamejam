308ae07ac707f97e665b4e648f273c770b5b91fe
David Hurst <sideshowdave7@gmail.com>
2013-09-22 09:31:25 -0600

    things now patrol after transition

---
+++ Assets/Scripts/LevelObjective.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class LevelObjective : MonoBehaviour
+{
+	// Use this for initialization
+	void Awake ()
+	{
+	}
+	
+	private enum Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{	
+		if (other.gameObject.tag == "Player") {
+			var diff = this.transform.position - other.transform.position;
+			diff.Normalize ();
+			ExitDirection dir;
+			if (diff.y < diff.x)
+				dir = ExitDirection.Right;
+			else if (diff.y < -diff.x)
+				dir = ExitDirection.Left;
+			else if (diff.y > 0f)
+				dir = ExitDirection.Top;
+			else
+				dir = ExitDirection.Bottom;
+			StartLevelTransition (dir);
+		}
+	}
+	
+	void StartLevelTransition (ExitDirection dir)
+	{
+		Vector3 exitDest, mid, enterSource;
+		mid = Vector3.zero;
+		var ll = GameObject.FindObjectOfType (typeof(LevelLoader)) as LevelLoader;
+		if (ll != null) {
+			switch (dir) {
+			case ExitDirection.Left:
+				exitDest = new Vector3 (20f, 0f, 0f);
+				enterSource = new Vector3 (-20f, 0f, 0f);
+				break;
+			case ExitDirection.Right:
+				exitDest = new Vector3 (-20f, 0f, 0f);
+				enterSource = new Vector3 (20f, 0f, 0f);
+				break;
+			case ExitDirection.Top:
+				exitDest = new Vector3 (0f, 0f, -15f);
+				enterSource = new Vector3 (-20f, 0f, 15f);
+				break;
+			default:
+				exitDest = new Vector3 (0f, 0f, 15f);
+				enterSource = new Vector3 (-20f, 0f, -15f);
+				break;
+			}
+		
+			var oldLvl = ll.lvl;
+			var player = GameObject.FindGameObjectWithTag("Player");
+			GameObject.Destroy(player);
+			var newLvl = ll.LoadNextLevel (enterSource);
+			StartCoroutine (LevelTransition (oldLvl, newLvl, exitDest));
+		}
+	}
+	
+	IEnumerator LevelTransition (GameObject oldLevel, GameObject newLevel, Vector3 exitDest)
+	{
+		const float speed = 15f;
+		while (newLevel.transform.position != Vector3.zero) {
+			oldLevel.transform.position = Vector3.MoveTowards (oldLevel.transform.position, exitDest, speed * Time.deltaTime);
+			newLevel.transform.position = Vector3.MoveTowards (newLevel.transform.position, Vector3.zero, speed * Time.deltaTime);
+			yield return 1;
+		}
+		
+		foreach (Progress progress in AstarPath.active.ScanLoop ()) {
+				Debug.Log ("Rescanning paths");
+		}
+		
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		
+		foreach (GameObject enemy in enemies) {
+			enemy.GetComponent<ActorControl>().isActive = true;
+		}
+		
+		GameObject.Destroy (oldLevel);
+	}
+	
+	private enum ExitDirection
+	{
+		Left,
+		Right,
+		Top,
+		Bottom
+	}
+}
---
+++ Assets/Scripts/Ogmo/OgmoLevel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class OgmoLevel
+{
+	public string name;
+	public int width;
+	public int height;
+	public Dictionary<string,OgmoLayer> layers;
+
+	public OgmoLevel (TextAsset xmlFile)
+	{
+		name = xmlFile.name;
+		XmlDocument xml = new XmlDocument ();
+		xml.LoadXml (xmlFile.text);
+		XmlElement root = (XmlElement)xml.FirstChild;
+		width = int.Parse (root.GetAttribute ("width"));
+		height = int.Parse (root.GetAttribute ("height"));
+		layers = new Dictionary<string,OgmoLayer> ();
+		foreach (XmlElement child in root) {
+			OgmoLayer layer = new OgmoLayer (child, width, height);
+			layers [layer.name] = layer;
+		}
+	}
+}
