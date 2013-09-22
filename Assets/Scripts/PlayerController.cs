04e9043a3a70630b8f749935f41fd92da696fa13
Andrew Jones <andrew.b.jones@gmail.com>
2013-09-22 08:26:03 -0600

    Fixed vignette at start of game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour
 	void Start ()
 	{
 		prevTogglePower = false;
-	}
-	
+		SetCurrentPower(PlayerPowerEnum.Visual);
+	}	
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,64 +23,8 @@ public class PlayerController : MonoBehaviour
 		var togglePower = Input.GetButtonDown ("TogglePower");
 		
 		if (togglePower && !prevTogglePower) {
-			//This state is now false (rising edge)
-			var enemies = GameObject.FindGameObjectsWithTag ("Enemy");
-			
-			switch (currentPower) {
-			case PlayerPowerEnum.Audio:
-				DarknessAudioManager.Instance.muteLayers (1f);
-				foreach (var enemy in enemies) {
-					enemy.GetComponent<AudioSource> ().pitch = .8f;	
-				}
-				currentPower = PlayerPowerEnum.Time;
-				break;
-			case PlayerPowerEnum.Time:
-				var times = GameObject.FindObjectsOfType (typeof(TimeToggleThingy));
-				foreach (TimeToggleThingy t in times) {
-					t.setTimeSlow (false);
-				}		
-				foreach (var enemy in enemies) {
-					enemy.GetComponent<SFXVolumeControl> ().sfxMultiplier = 1.2f;
-					enemy.GetComponent<AIPath> ().speed *= 3;
-				}
-				currentPower = PlayerPowerEnum.Visual;
-				break;
-			case PlayerPowerEnum.Visual:
-				var visuals = GameObject.FindObjectsOfType (typeof(VisibleToggleSpritesThingy));
-				foreach (VisibleToggleSpritesThingy vis in visuals) {
-					vis.SetVisibleMode (false);
-				}
-				StartCoroutine (FadeVisual (0f, 1f));
-				currentPower = PlayerPowerEnum.Time;
-				break;
-			}
-			
-			//This state is now true (rising edge)
-			switch (currentPower) {
-			case PlayerPowerEnum.Audio:
-				DarknessAudioManager.Instance.muteLayers (.1f);
-				foreach (var enemy in enemies) {
-					enemy.GetComponent<AudioSource> ().pitch = 1f;
-				}
-				break;
-			case PlayerPowerEnum.Time:
-				var times = GameObject.FindObjectsOfType (typeof(TimeToggleThingy));
-				foreach (TimeToggleThingy t in times) {
-					t.setTimeSlow (true);
-				}
-				foreach (var enemy in enemies) {
-					enemy.GetComponent<SFXVolumeControl> ().sfxMultiplier = 2.5f;
-					enemy.GetComponent<AIPath> ().speed *= 1 / 3f;
-				}
-				break;
-			case PlayerPowerEnum.Visual:
-				var visuals = GameObject.FindObjectsOfType (typeof(VisibleToggleSpritesThingy));
-				foreach (VisibleToggleSpritesThingy vis in visuals) {
-					vis.SetVisibleMode (true);
-				}
-				StartCoroutine (FadeVisual (1f, 0f));
-				break;
-			}
+			SetCurrentPower (
+				(PlayerPowerEnum)(((int)currentPower + 1) % ((int)PlayerPowerEnum.End)));
 		}
 		
 		this.rigidbody.velocity = (Vector3.Normalize (input) * move_speed);
@@ -103,7 +47,64 @@ public class PlayerController : MonoBehaviour
 		vignette.alpha = end;
 	}
 	
-	
+	void SetCurrentPower (PlayerPowerEnum power)
+	{
+		//This state is now false (rising edge)
+		var enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		switch (currentPower) {
+		case PlayerPowerEnum.Audio:
+			DarknessAudioManager.Instance.muteLayers (1f);
+			foreach (var enemy in enemies) {
+				enemy.GetComponent<AudioSource> ().pitch = .8f;	
+			}
+			break;
+		case PlayerPowerEnum.Time:
+			var times = GameObject.FindObjectsOfType (typeof(TimeToggleThingy));
+			foreach (TimeToggleThingy t in times) {
+				t.setTimeSlow (false);
+			}		
+			foreach (var enemy in enemies) {
+				enemy.GetComponent<SFXVolumeControl> ().sfxMultiplier = 1.2f;
+				enemy.GetComponent<AIPath> ().speed *= 3;
+			}
+			break;
+		case PlayerPowerEnum.Visual:
+			var visuals = GameObject.FindObjectsOfType (typeof(VisibleToggleSpritesThingy));
+			foreach (VisibleToggleSpritesThingy vis in visuals) {
+				vis.SetVisibleMode (false);
+			}
+			StartCoroutine (FadeVisual (0f, 1f));
+			break;
+		}
+		
+		currentPower = power;
+		//This state is now true (rising edge)
+		switch (currentPower) {
+		case PlayerPowerEnum.Audio:
+			DarknessAudioManager.Instance.muteLayers (.1f);
+			foreach (var enemy in enemies) {
+				enemy.GetComponent<AudioSource> ().pitch = 1f;
+			}
+			break;
+		case PlayerPowerEnum.Time:
+			var times = GameObject.FindObjectsOfType (typeof(TimeToggleThingy));
+			foreach (TimeToggleThingy t in times) {
+				t.setTimeSlow (true);
+			}
+			foreach (var enemy in enemies) {
+				enemy.GetComponent<SFXVolumeControl> ().sfxMultiplier = 2.5f;
+				enemy.GetComponent<AIPath> ().speed *= 1 / 3f;
+			}
+			break;
+		case PlayerPowerEnum.Visual:
+			var visuals = GameObject.FindObjectsOfType (typeof(VisibleToggleSpritesThingy));
+			foreach (VisibleToggleSpritesThingy vis in visuals) {
+				vis.SetVisibleMode (true);
+			}
+			StartCoroutine (FadeVisual (1f, 0f));
+			break;
+		}
+	}
 	
 	public enum PlayerPowerEnum
 	{
