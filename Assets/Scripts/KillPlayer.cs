e757153c7f99a9835adac02740b30dfb4e4ed005
David Hurst <sideshowdave7@gmail.com>
2013-09-22 06:12:41 -0600

    more fixes

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class KillPlayer : MonoBehaviour {
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	void OnTriggerEnter(Collider other) {		
+        if(other.gameObject.tag == "Player"){
+			var ll = GameObject.FindObjectOfType(typeof(LevelLoader)) as LevelLoader;
+			if(ll!=null)
+				ll.ReloadLevel();
+		}
+    }
+}
---
+++ Assets/Scripts/TimeToggleThingy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(EnemyBehaviour))]
+public class TimeToggleThingy : MonoBehaviour
+{
+	public float timeSlowMultiplier;
+	private bool isSlowed = false;
+	
+	public void setTimeSlow (bool enabled)
+	{
+		if (enabled && !isSlowed) {
+			EnemyBehaviour eb = GetComponent<EnemyBehaviour> ();
+			eb.speed *= timeSlowMultiplier;
+			isSlowed = true;
+		} else if (!enabled && isSlowed) {
+			EnemyBehaviour eb = GetComponent<EnemyBehaviour> ();
+			eb.speed /= timeSlowMultiplier;
+			isSlowed = false;			
+		}
+	}
+}
