131fb2bc91d2b142f3a1e3de57923a23cf54ca32
Brian Paulsen <Brian_Paulsen123@hotmail.com>
2013-09-22 06:48:41 -0600

    patrol/ pursue

---
+++ Assets/ActorControl.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+public class ActorControl : MonoBehaviour {
+
+	public
+		Vector3 position, velocity;
+	public
+		float viewDistance = 0, countDown;
+	public
+		int aware, health, speed, behavior, damage; 
+		
+	public
+		GameObject[] navPoints;
+	
+	protected
+		GameObject[] tempNavPoints;
+	protected 
+		float timeKeeper;
+	protected
+		GameObject Actor, Player;
+		
+	
+	 void Start(){	
+	 	this.position = transform.position;
+		Player = GameObject.FindGameObjectWithTag( "Player" );
+	}
+
+	void detect(){
+		if( Player == null )
+			return;
+		RaycastHit hitInfo;
+		bool isHit;
+		Ray test = new Ray( collider.bounds.center,  Player.collider.bounds.center - collider.bounds.center );
+		
+		isHit = Physics.Raycast ( test, out hitInfo, viewDistance );
+		
+		if( isHit && hitInfo.collider.tag == "Player" )
+		{
+			setAware( 1 );
+			timeKeeper = 0;
+		}
+		else setAware( 0 );
+		
+		if( aware == 1 )
+			behavior = 1;
+	}
+
+	void move(){
+		switch( behavior )
+		{
+		case 0:
+			
+			gameObject.SendMessage( "AIPathCall", navPoints , SendMessageOptions.DontRequireReceiver );
+				break;
+		case 1:
+			tempNavPoints = new GameObject[1];
+			tempNavPoints[0] = Player;
+			gameObject.SendMessage( "AIPathCall", tempNavPoints , SendMessageOptions.DontRequireReceiver );
+				break;
+		}	
+	}
+	
+	void attack(){
+		// get proximity to player
+		// if( proximity <= range )
+		// damagePlayer();
+	}
+
+	void  coolOff(){
+		if( aware == 0 && behavior == 1 ){
+			timeKeeper += Time.deltaTime;
+			if(timeKeeper > countDown )
+			{
+				behavior = 0;
+				timeKeeper = 0;
+			}
+		}
+		else
+			timeKeeper = 0;
+	}
+
+	int  getAware(){
+		return this.aware;
+	}
+
+	void  setAware( int aware ){
+		this.aware = aware;
+	}
+
+	int  getHealth(){
+		return this.health;
+	}
+
+	void  setHealth( int health ){
+		this.health = health;
+	}
+
+	int  getDamage(){
+		return this.damage;
+	}
+
+	void  setDamage( int damage ){
+		this.damage = damage;
+	}
+
+	int  getSpeed(){
+		return this.speed;
+	}
+
+	void  setSpeed( int speed ){
+		this.speed = speed;
+	}
+
+	int  getBehavior(){
+		return this.behavior;
+	}
+
+	void  setBehavior( int behavior ){
+		this.behavior = behavior;
+	}
+
+
+	void Update () {
+		if( Player == null )
+			Player = GameObject.FindGameObjectWithTag( "Player" );
+		
+		detect();
+		move();
+		attack();
+		coolOff();
+	}
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LevelLoader : MonoBehaviour
+{	
+	private int levelIndex = 0;
+	public TextAsset[] levels;
+	public string zoneFolder;
+	public GameObject lvl;
+	
+	void Awake ()
+	{
+		if (levels != null && levels.Length > 0) {
+			ReloadLevel ();
+		}
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.L)) {
+			GameObject.Destroy (lvl);
+			LoadNextLevel (Vector3.zero);
+		}
+	}
+	
+	private GameObject LoadLevel (int index, Vector3 pos)
+	{
+		levelIndex = Mathf.Clamp(index, 0, levels.Length -1);
+		var level = new OgmoLevel (levels [levelIndex]);
+		lvl = new GameObject ("TheLevelStuff");
+		lvl.transform.position = pos;
+		foreach (var layer in level.layers.Values) {
+			LoadLayer (layer);
+		}
+		return lvl;
+	}
+	
+	public GameObject LoadNextLevel (Vector3 pos)
+	{
+		return LoadLevel (++levelIndex, pos);
+	}
+	
+	public GameObject ReloadLevel ()
+	{
+		return LoadLevel (levelIndex, Vector3.zero);
+	}
+	
+	private void LoadLayer (OgmoLayer layer)
+	{
+		if (layer.tiles != null) {
+			float height = 0f;
+			switch (layer.name) {
+			case "Foreground":
+				height = 5f;
+				break;
+			case "Background":
+				height = 0f;
+				break;
+			}
+			foreach (var tile in layer.tiles) {
+				LoadTile (tile, height);
+			}
+		}
+		if (layer.entities != null) {
+			foreach (var entity in layer.entities) {
+				LoadEntity (entity);
+			}
+		}
+	}
+	
+	private void LoadTile (OgmoTile tile, float height)
+	{
+		string tileName = string.Format ("frame{0}", tile.id);
+		string tilePath = (zoneFolder ?? "") + tileName;
+		var go = GetPrefab (tilePath);
+		if (go != null) {
+			go = (GameObject)GameObject.Instantiate (go);
+			go.transform.parent = lvl.transform;
+			OTObject ot = go.GetComponent<OTObject> ();
+			if (ot != null)
+				ot.position = new Vector2 (tile.x, 15 - tile.y);
+			else
+				go.transform.localPosition = new Vector3 (tile.x, height, -tile.y);
+		}
+	}
+	
+	private void LoadEntity (OgmoEntity entity)
+	{
+		var go = GetPrefab (entity.name);
+		if (go != null) {
+			go = (GameObject)GameObject.Instantiate (go);
+			go.transform.parent = lvl.transform;
+			OTObject ot = go.GetComponent<OTObject> ();
+			go.transform.localPosition = new Vector3 (((float)entity.x) / 16f, 0.25f, 15f - ((float)entity.y) / 16f);
+			if (ot != null)
+				ot.position = new Vector2 (go.transform.position.x, go.transform.position.z);
+		}
+	}
+	
+	private GameObject GetPrefab (string path)
+	{
+		UnityEngine.Object go;
+		go = Resources.Load (path);
+		return (go as GameObject);
+	}
+}
