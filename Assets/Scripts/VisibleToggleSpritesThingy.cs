ff253782711171d28b0851de18e5a5a16b569e31
Andrew Jones <andrew.b.jones@gmail.com>
2013-09-22 08:06:21 -0600

    More tweaking visuals

---
+++ Assets/Scripts/VisibleToggleSpritesThingy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(OTSprite))]
+public class VisibleToggleSpritesThingy : MonoBehaviour
+{
+	public int visibleOffset;
+	
+	public void SetVisibleMode (bool visible)
+	{
+		var sprite = GetComponent<OTSprite> ();
+		/*
+		if (visible && sprite.frameIndex < visibleOffset) {
+			sprite.frameIndex += visibleOffset;
+		}else if(!visible && sprite.frameIndex >= visibleOffset){
+			sprite.frameIndex -= visibleOffset;
+		}*/
+		sprite.visible = !visible;
+	}
+}
