64ad41b06bbc511b7da5dbb0f0b58d0caf1e599e
Andrew Jones <andrew.b.jones@gmail.com>
2013-09-22 17:08:53 -0600

    2 states, set enemies to active on death

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationStateController : MonoBehaviour {
+	
+	
+	public OTAnimatingSprite animSprite;
+	Vector3 dir;
+	
+	// Use this for initialization
+	void Start () {
+		animSprite = this.gameObject.GetComponent<OTAnimatingSprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		dir = Vector3.Normalize(this.rigidbody.velocity);	
+		
+		var lmc = this.gameObject.GetComponent<LargeMovementControl>();
+		if (lmc != null){
+			
+			dir = lmc.dir;
+		}
+		
+		if (dir == Vector3.zero){
+			animSprite.animationFrameset = "idle";
+		} else if (dir == Vector3.right){
+			animSprite.animationFrameset = "right";
+		} else if (dir == Vector3.left){
+			animSprite.animationFrameset = "left";
+		} else if (dir == Vector3.forward){
+			animSprite.animationFrameset = "forward";
+		} else if (dir == Vector3.back){
+			animSprite.animationFrameset = "back";
+		} 
+	}
+}
---
+++ Assets/Flash.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flash : MonoBehaviour {
+	
+	private GUITexture flash;
+	Color flashColor = Color.black;
+	
+	// Use this for initialization
+	void Start () {
+		Texture2D tex= new Texture2D ( 1 , 1 );
+	    tex.SetPixel( 0 , 0 , Color.white );
+	    tex.Apply();
+	 
+	    var storageGB = new GameObject("Flash");
+	    storageGB.transform.localScale = new Vector3(0 , 0 , 1);
+	 
+	    flash = storageGB.AddComponent<GUITexture>();
+	    flash.pixelInset = new Rect(0 , 0 , Screen.width , Screen.height );
+	    flash.color = flashColor;
+	    flash.texture = tex;
+	    flash.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
+
