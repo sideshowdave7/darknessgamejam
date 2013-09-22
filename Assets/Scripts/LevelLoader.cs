45dbd64b0726b2e910e179afc0bf959af37f0e23
David Hurst <sideshowdave7@gmail.com>
2013-09-22 04:31:39 -0600

    AUTO gen astar paths

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -34,6 +34,10 @@ public class LevelLoader : MonoBehaviour
 		foreach (var layer in level.layers.Values) {
 			LoadLayer (layer);
 		}
+		var astar = GameObject.FindGameObjectWithTag("Astar");
+		if (astar != null){
+			astar.GetComponent<AstarPath>().Scan();	
+		}
 		return lvl;
 	}
 	
@@ -83,6 +87,7 @@ public class LevelLoader : MonoBehaviour
 				ot.position = new Vector2 (tile.x, 15 - tile.y);
 			else
 				go.transform.position = new Vector3 (tile.x, height, -tile.y);
+			
 		}
 	}
 	
