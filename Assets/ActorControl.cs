b5962263c7919eecff09b07d747da15e2778536b
Andrew Jones <andrew.b.jones@gmail.com>
2013-09-22 04:53:02 -0600

    Added Exit, level transitions

---
+++ Assets/ActorControl.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+public class ActorControl : MonoBehaviour {
+
+	public
+		Vector3 position, velocity;
+	public
+		float viewDistance = 0;
+	public
+		int aware, health, speed, behavior, damage, countDown, ignoreThis;	
+		GameObject Actor, Player;
+		
+	
+	 void Start(){
+		//Get player object
+		
+	 	this.position = transform.position;
+		Player = GameObject.FindGameObjectWithTag( "Player" );
+		
+	}
+
+	void detect(){
+
+		RaycastHit hitInfo;
+		bool isHit;
+		Ray test = new Ray( collider.bounds.center,  Player.collider.bounds.center - collider.bounds.center );
+		
+		isHit = Physics.Raycast ( test, out hitInfo, viewDistance );
+		
+		if( isHit && hitInfo.collider.tag == "Player" ) setAware( 1 );
+		else setAware( 0 );
+		
+		if( aware == 1 )
+			behavior = 1;
+	}
+
+	void move(){
+		switch( behavior )
+		{
+			case 0: //some passive stuff 
+				break;
+			case 1: gameObject.SendMessage ( "AIPathCall", SendMessageOptions.DontRequireReceiver );
+				break;
+			
+		}
+	}
+
+	void attack(){
+		// get proximity to player
+		// if( proximity <= range )
+		// damagePlayer();
+	}
+
+	void  coolOff(){
+		if( aware == 0 && behavior == 1 && ignoreThis >= countDown )
+			ignoreThis = 0;
+		if( ignoreThis <= countDown )
+			ignoreThis++;
+		else behavior = 0;
+	}
+
+	int  getAware(){
+		return this.aware;
+	}
+
+	void  setAware( int aware ){
+		this.aware = aware;
+	}
+
+	int  getHealth(){
+		return this.health;
+	}
+
+	void  setHealth( int health ){
+		this.health = health;
+	}
+
+	int  getDamage(){
+		return this.damage;
+		//return this;
+	}
+
+	void  setDamage( int attack ){
+		//this.attack = attack;
+	}
+
+	int  getSpeed(){
+		return this.speed;
+	}
+
+	void  setSpeed( int speed ){
+		this.speed = speed;
+	}
+
+	int  getBehavior(){
+		return this.behavior;
+	}
+
+	void  setBehavior( int behavior ){
+		this.behavior = behavior;
+	}
+
+
+	void Update () {
+		if( Player == null )
+			Player = GameObject.FindGameObjectWithTag( "Player" );
+		
+		detect();
+		move();
+		attack();
+		coolOff();
+	}
+}
